dbeae8abae825ce5cb57963fcd74a34f47831173
Byron Schaller <byron.schaller@gmail.com>
2016-12-14 00:46:27 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# kapacitor
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'kapacitor'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures kapacitor'
+long_description 'Installs/Configures kapacitor'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/kapacitor/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/kapacitor' if respond_to?(:source_url)
